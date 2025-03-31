2ed3ec113c6d559ce59f80703a006dbfdf4fd7bc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 09:19:58 -0300

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(alert("Bem vindo ao site!"))
+var nome = prompt("Digite o seu nome abaixo:")
+alert("Olá, " + nome)
+var idade = parseInt(prompt("Digite a sua idade abaixo: "))
+
+var anos = idade + 10
+alert("A sua idade daqui dez anos será de " + anos + " anos")
+
