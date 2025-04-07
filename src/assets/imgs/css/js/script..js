5aa1d15d5f5adc20a0e1924917da4355ffb7e9c4
Artur Rodrigues <arturrodriguestpaes@gmail.com>
2025-04-07 08:34:33 -0300

    commit

---
+++ src/assets/imgs/css/js/script..js
@@ -0,0 +1,7 @@
+var nomeUsuario = prompt("Inserir nome de usuário: ");
+if (nomeUsuario == ""){
+    alert("O nome de usuário não foi inserido");
+}
+else{
+    alert("Nome de usuário inserido: "+ nomeUsuario );
+}
