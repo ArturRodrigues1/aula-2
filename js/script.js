91dddda648bde7a422110830c91d4f02662c507f
Artur Rodrigues <arturrodriguestpaes@gmail.com>
2025-04-07 08:49:35 -0300

    c1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// var nomeUsuario = prompt("Inserir nome de usuário: ");
+// if (nomeUsuario == ""){
+//     alert("O nome de usuário não foi inserido");
+// }
+// else{
+//     alert("Nome de usuário inserido: "+ nomeUsuario );
+// }
+
+var combustivel = "Gasolina"
+
+if (combustivel == "GNV"){
+    alert("O carro é movido á gás")
+}
+else if (combustivel == "Gasolina"){
+    alert ("O carro é movido a gasolina")
+}
+else if(combustivel == "Álcool"){
+    alert("O carro é movido a alcool")
+}
+else {
+    alert("O carro não funciona")
+}
--- src/assets/imgs/css/js/script..js
+++
@@ -1,7 +0,0 @@
-var nomeUsuario = prompt("Inserir nome de usuário: ");
-if (nomeUsuario == ""){
-    alert("O nome de usuário não foi inserido");
-}
-else{
-    alert("Nome de usuário inserido: "+ nomeUsuario );
-}
