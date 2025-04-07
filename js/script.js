8efd2d4ca90e294905053cb748d9be8ced2e6c63
Artur Rodrigues <arturrodriguestpaes@gmail.com>
2025-04-07 09:47:50 -0300

    comit

--- js/script.js
+++ js/script.js
@@ -6,17 +6,99 @@
 //     alert("Nome de usuário inserido: "+ nomeUsuario );
 // }
 
-var combustivel = "Gasolina"
 
-if (combustivel == "GNV"){
-    alert("O carro é movido á gás")
-}
-else if (combustivel == "Gasolina"){
-    alert ("O carro é movido a gasolina")
-}
-else if(combustivel == "Álcool"){
-    alert("O carro é movido a alcool")
-}
-else {
-    alert("O carro não funciona")
-}
+// var combustivel = "Gasolina"
+
+// if (combustivel == "GNV"){
+//     alert("O carro é movido á gás")
+// }
+// else if (combustivel == "Gasolina"){
+//     alert ("O carro é movido a gasolina")
+// }
+// else if(combustivel == "Álcool"){
+//     alert("O carro é movido a alcool")
+// }
+// else {
+//     alert("O carro não funciona")
+// }
+
+// var numero = prompt("Digite um número de 1 a 5: ") 
+// if (numero == 1){
+//     alert("O numero é 1")
+// }
+// else if (numero == 2){
+//     alert("O número é 2")
+// }
+// else if (numero == 3){
+//     alert("O numero é 3")
+// }
+    
+// else if(numero == 4){
+//         alert("O número é 4")
+//     }
+// else if (numero == 5){
+//     alert("O número é 5")
+// }
+// else{
+//     alert("Numero inválido  ")
+// }
+
+
+// var numero = prompt("Digite um número de 1 a 100: ")
+// if (numero>1 & numero < 11){ 
+//     alert ("O número está entre 1 e 10.")
+// }
+// else if (numero> 10 & numero < 21)
+//     alert("O número está entre 11 e 20.")
+// else if (numero> 20 & numero < 31)
+//     alert("O número está entre 20 e 30.")
+
+// else if (numero> 30 & numero < 41)
+//     alert("O número está entre 30 e 40.")
+
+// else  (numero> 80 & numero < 91)
+//     alert("O número está entre 80 e 90.")
+
+// else if (numero> 90 & numero < 101)
+//     alert("O número está entre 90 e 100.")
+
+// else{
+//     alert("Número inválido")
+// }if (numero> 40 & numero < 51)
+//     alert("O número está entre 40 e 50.")
+
+// else if (numero> 50 & numero < 61)
+//     alert("O número está entre 50 e 60.")
+
+// else if (numero> 60 & numero < 71)
+//     alert("O número está entre 60 e 70.")
+
+// else if (numero> 70 & numero < 81)
+//     alert("O número está entre 70 e 80.")
+
+// else if
+
+// var nome = prompt("Inserir nome: ")
+// var sobrenome = prompt("Digite o seu sobrenome: ")
+// if ((nome != "")&&(sobrenome != "")){
+//     alert("Nome: " + nome + "\n Sobrenome: " + 
+// sobrenome)
+// }else {
+//     alert("Erro ao inserir nome e sobrenome")
+// }
+
+// var nome = prompt("Insira seu nome: ")
+// if((nome == "ANA")||(nome == "ANA")||(nome =="Ana"))
+// {
+//     alert("O nome inserido é Ana")
+// } else{
+//     alert("O nome inserido não é Ana.")
+// }
+
+// var nome = prompt("Inserir nome")
+// var eVazio = nome == ""
+// if(!eVazio){
+//     alert("Nome: " + nome)
+// }else {
+//     alert("Nome é vazio")
+// }
